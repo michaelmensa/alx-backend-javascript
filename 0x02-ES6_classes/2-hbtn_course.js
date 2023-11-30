81bfb8520a8c101b8cb3cf4d4093eb4a2261ad48
michaelmensa <michaelmensa@live.com>
2023-11-30 12:08:53 +0000

    task2: using getters and setters. used throw new error to catch errors

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,47 @@
+/* eslint-disable no-underscore-dangle */
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  // Getter for name
+  get name() {
+    return this._name;
+  }
+
+  // Setter for name
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = name;
+  }
+
+  // Getter for length
+  get length() {
+    return this._length;
+  }
+
+  // Setter for length
+  set length(length) {
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a string');
+    }
+    this._length = length;
+  }
+
+  // Getter for students
+  get students() {
+    return this._students;
+  }
+
+  // Setter for students
+  set students(students) {
+    if (Array.isArray(students) !== true) {
+      throw new TypeError('Students must be an array');
+    }
+    this._students = students;
+  }
+}
