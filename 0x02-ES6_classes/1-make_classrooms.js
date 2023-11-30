b6320e8094c9e1f3476df812f47d7d01f84303d1
michaelmensa <michaelmensa@live.com>
2023-11-30 12:05:49 +0000

    task1: function to initialize class

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-underscore-dangle */
+import ClassRoom from './0-classroom';
+
+export default function initializeRooms() {
+  const rooms = [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
+  return rooms;
+}
