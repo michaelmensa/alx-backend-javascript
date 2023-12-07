d64e9e56761df1caf6f906651cd71f36afcb10cc
michaelmensa <michaelmensa@live.com>
2023-12-07 00:14:02 +0000

    task 1

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,9 @@
+// define the teacher interface
+export interface Teacher {
+    firstName: string;
+    lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    location: string;
+    [key: string]: any; // allow addtional attributes
+}
