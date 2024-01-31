81cd0f58407720bdda4bd3f62814a97a54adc64f
michaelmensa <michaelmensa@live.com>
2024-01-31 22:27:54 +0000

    test2 chai

---
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -0,0 +1,20 @@
+function calculateNumber(type, a, b) {
+  const roundA = Math.round(a);
+  const roundB = Math.round(b);
+  if (type === 'SUM') {
+    return roundA + roundB;
+  }
+  else if (type === 'SUBTRACT') {
+    return roundA - roundB;
+  }
+  else if (type === 'DIVIDE') {
+    if (roundB === 0) {
+      return 'Error';
+    }
+    else {
+      return roundA / roundB;
+    }
+  }
+}
+
+module.exports = calculateNumber;
---
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,28 @@
+const calculateNumber = require("./2-calcul_chai");
+
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('calculateNumber', () => {
+  describe('calculateNumber SUM', () => {
+    it('sum two numbers', () => {
+      expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
+    });
+  });
+  describe('calculateNumber SUBTRACT', () => {
+    it('subtract two numbers', () => {
+      expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
+    });
+  });
+  describe('calculateNumber DIVIDE', () => {
+    it('divide two numbers', () => {
+      expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
+    });
+  });
+  describe('calculateNumber DIVIDE BY 0', () => {
+    it('DIVIDES BY 0', () => {
+      expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal('Error');
+    });
+  });
+
+});
