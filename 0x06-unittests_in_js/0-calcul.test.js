21258537168c2f140413edc3113dbcd331ba0935
michaelmensa <michaelmensa@live.com>
2024-01-31 16:16:16 +0000

    test0

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,20 @@
+const assert = require("assert");
+const calculateNumber = require("./0-calcul");
+
+describe('calculateNumber', () => {
+  it('should sum two whole numbers',() => {
+    assert.strictEqual(calculateNumber(1, 4), 5);
+  });
+  it('should round and sum two numbers',() => {
+    assert.strictEqual(calculateNumber(1, 4.2), 5);
+  });
+  it('should round and sum two numbers',() => {
+    assert.strictEqual(calculateNumber(1.7, 4), 6);
+  });
+  it('should round and sum one negative and one positive number',() => {
+    assert.strictEqual(calculateNumber(1.7, -4), -2);
+  });
+  it('should sum 0 and return 0',() => {
+    assert.strictEqual(calculateNumber(0, 0), 0);
+  });
+});
